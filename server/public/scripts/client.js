3f90446181f95f80e444ad60916a747907996ff0
lawlernp <lawlernp@gmail.com>
2020-10-19 04:37:23 -0500

    PUT request working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,6 +2,9 @@ $(document).ready(onReady)
 function onReady() {
     console.log('jq');
     $('#buttonSubmit').on('click', submitTask);
+    $('#listBody').on('click', '.completeBtn', completeTask);
+    $('#listBody').on('click', '.deleteBtn', deleteTask);
+
     getTasks();
 }
 
@@ -29,6 +32,7 @@ function getTasks() {
         console.log("skjfhsd", response);
         // append data to the DOM
         for (let i = 0; i < response.length; i++) {
+            if (response[i].complete){
             $('#listBody').append(`
                 <tr data-id=${response[i].id}>
                     <td>${response[i].name}</td>
@@ -37,9 +41,56 @@ function getTasks() {
                     <td>
                         <input type="checkbox" id="completed" checked>
                     </td>
-                    </tr>
-            `);
+                </tr>
+            `);}
+            else{
+            $('#listBody').append(`
+                <tr data-id=${response[i].id}>
+                    <td>${response[i].name}</td>
+                    <td>${response[i].complete}</td>
+                    <td><button class="deleteBtn">delete</button></td>
+                    <td><button data-completeStatus=${response[i].complete} class="completeBtn">complete</button></td>
+                    <td>
+                        <input type="checkbox" id="completed">
+                    </td>
+                </tr>
+            `);}
         }
     });
 };
+
+function deleteTask(){
+    console.log('clicked');
+    let taskId = $(this).closest('tr').data('id');
+    console.log(taskId);
+    // this is where I was to fire an ajax request - to DELETE
+    $.ajax({
+        method: 'DELETE',
+        url: `/taskList/${taskId}`
+    }).then( function(response){
+        console.log(response);
+        getTasks();
+    }).catch(function(error){
+        console.log('error', error);
+    });
+
+}
+
+function completeTask(){
+    let completeStatus = $(this).data('completeStatus');
+    let taskId = $(this).closest('tr').data('id');
+    console.log("clicked", completeStatus, taskId);
+
+    $.ajax({
+        method: 'PUT',
+        url: `/taskList/complete/${taskId}`,
+        data: {complete: completeStatus}
+    }).then(function (response){
+        console.log('response', response);
+        getTasks();
+    }).catch(function(error){
+        console.log('error in put', error);
+    });
+
+}
 console.log('js')
--- server/routes/task_router.js
+++ server/routes/task_router.js
@@ -29,6 +29,34 @@ router.post('/', (req, res) => {
     })
 });
 
+router.delete('/:idParam', (req, res) => {
+    console.log("hello from DELETE", req.params.idParam);
+    // res.sendStatus(200);
+    let queryText = `DELETE FROM "tasks" WHERE "id" = $1`;
+
+    pool.query(queryText, [req.params.idParam]).then((result)=>{
+        console.log("Success! ", result);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('error in delete', error);
+        res.sendStatus(500);
+    });
+});
+
+router.put('/complete/:idParam', (req,res) => {
+    console.log('in put request', req.body.completeStatus, req.params.idParam );
+    let queryText = `UPDATE "tasks" SET "complete" = true WHERE "id" = $1;`;
+  
+    pool.query(queryText, [req.params.idParam]).then((result) => {
+        console.log('result from put', result);
+        res.sendStatus(200);
+    }).catch((error) => {
+        console.log('error in put', error);
+        res.sendStatus(500);
+    });
+
+});
+
 
 
 module.exports = router;
